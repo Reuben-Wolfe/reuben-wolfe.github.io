3948854fba1f163f986a0d427df78a9e9fb940f4
Reuben-Wolfe <reuben.218336@nv.ccsd.net>
2017-03-23 09:05:58 -0700

    Add files via upload

---
+++ JS/java.js
@@ -0,0 +1,12 @@
+<script>
+  addEventListener.getElementsById('book-cart').addEventListener('submit, estimateTotal')
+  function estimateTotal(event) {
+  event.preventDefault();
+      
+  var state = (document.getElementById('s-state')
+  if (state.value === '') {
+  alert('Please choose your shipping state.');
+ state.focus();
+          }
+          } 
+</script>
